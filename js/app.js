e3847765c709ee1bfb25dd75e8334c99d364c1e9
Ken HIll <ken.hill30@gmail.com>
2020-08-26 18:08:37 -0400

    Added fadein on scroll to product cards. Added focus to product images

--- js/app.js
+++ js/app.js
@@ -25,3 +25,63 @@ function loadHeroCTAAppear(){
 }
 
 window.addEventListener('load', loadHeroCTAAppear);
+
+//555555555
+
+function scrollProdTrackAppear(){
+    var prodTrackCard = document.querySelector('.product-track-hide');
+    var prodTrackPosition = prodTrackCard.getBoundingClientRect().top;
+    var screenHeight = window.innerHeight / 1.3;
+
+    if(prodTrackPosition < screenHeight)
+    {
+        prodTrackCard.classList.add('product-track-appear');
+    }
+}
+
+window.addEventListener('scroll', scrollProdTrackAppear);
+
+//666666666
+
+function scrollProdReliableAppear(){
+    var prodReliableCard = document.querySelector('.product-reliable-hide');
+    var prodReliablePosition = prodReliableCard.getBoundingClientRect().top;
+    var screenHeight = window.innerHeight / 1.3;
+
+    if(prodReliablePosition < screenHeight)
+    {
+        prodReliableCard.classList.add('product-reliable-appear');
+    }
+}
+
+window.addEventListener('scroll', scrollProdReliableAppear);
+
+//333333333
+
+function scrollProdEverythingAppear(){
+    var prodEverythingCard = document.querySelector('.product-everything-hide');
+    var prodEverythingPosition = prodEverythingCard.getBoundingClientRect().top;
+    var screenHeight = window.innerHeight / 1.3;
+
+    if(prodEverythingPosition < screenHeight)
+    {
+        prodEverythingCard.classList.add('product-everything-appear');
+    }
+}
+
+window.addEventListener('scroll', scrollProdEverythingAppear);
+
+//22222
+
+function scrollProdAdvancedAppear(){
+    var prodAdvancedCard = document.querySelector('.product-advanced-hide');
+    var prodAdvancedPosition = prodAdvancedCard.getBoundingClientRect().top;
+    var screenHeight = window.innerHeight / 1.3;
+
+    if(prodAdvancedPosition < screenHeight)
+    {
+        prodAdvancedCard.classList.add('product-advanced-appear');
+    }
+}
+
+window.addEventListener('scroll', scrollProdAdvancedAppear);
